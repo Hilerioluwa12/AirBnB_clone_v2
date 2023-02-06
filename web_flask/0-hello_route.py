fb0381ff050daa18f01739abee810f103cd065d1
hilerioluwa12 <hardexilerioluwa@gmail.com>
2023-02-06 17:04:25 +0000

    update

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,15 +3,17 @@
 """
 from flask import Flask
 app = Flask(__name__)
-# condition strict_slashes=Flase
+""" condition strict_slashes=Flase """
 app.url_map.strict_slashes = False
 
 
 @app.route('/')
 def hello():
-	""" print this message """
+	""" print this message
+	"""
 	return "Hello HBNB!"
 
 if __name__ == "__main__":
-	# config the run
+	""" config the run
+	"""
 	app.run(host='0.0.0.0', port=5000)
