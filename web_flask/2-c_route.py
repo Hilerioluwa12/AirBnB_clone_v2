90cc6809041c13c2a64777495d6a44afde06b0bf
hilerioluwa12 <hardexilerioluwa@gmail.com>
2023-02-06 18:17:08 +0000

    update

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -2,9 +2,9 @@
 """ start a flask web application.
 The application listens on 0.0.0.0, port 5000.
 Routes:
-	/: Displays 'Hello HBNB!'.
-	/hnh: Displays 'HBNB'.
-	/c/<text>: Displays 'C' followed by the value of <text>.
+    /: Displays 'Hello HBNB!'.
+    /hnh: Displays 'HBNB'.
+    /c/<text>: Displays 'C' followed by the value of <text>.
 """
 from flask import Flask
 
@@ -15,24 +15,24 @@ app.url_map.strict_slashes = False
 
 @app.route("/")
 def hello():
-	""" print this message
-	"""
-	return "Hello HBNH!"
+    """ print this message
+    """
+    return "Hello HBNH!"
 
 
 @app.route("/hbnb")
 def hbnb():
-	""" Display 'HBNB' """
-	return "HBNB"
+    """ Display 'HBNB' """
+    return "HBNB"
 
 
 @app.route("/c/<text>")
 def c(text):
-	""" displays 'c' then the value of <text>. 
-	"""
-	text = text.replace("_", " ")
-	return "C {}".format(text)
+    """ displays 'c' then the value of <text>. 
+    """
+    text = text.replace("_", " ")
+    return "C {}".format(text)
 
 
 if __name__ == "__main__":
-	app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0")
