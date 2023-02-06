8d66488ef900af858b19a2205348105defb3522f
hilerioluwa12 <hardexilerioluwa@gmail.com>
2023-02-06 20:10:37 +0000

    5-number_template.py

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -6,6 +6,8 @@ Routes:
     /hbnb: Displays 'HBNB'.
     /c/<text>: Displays 'C' followed by the value of <text>.
     /python/(<text>): Displays 'Python' followed by the value of <text>.
+    /number/<n>: Displays 'n is a number' only if <n> is an integer.
+    /number_template/<n>: Displays an HTML page only if <n> is an integer.
 """
 from flask import Flask
 from flask import render_template
@@ -15,19 +17,19 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello_hbnb():
-    """Displays 'Hello HBNB!'."""
+    """Displays 'Hello HBNB!'"""
     return "Hello HBNB!"
 
 
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
-    """Displays 'HBNB'."""
+    """Displays 'HBNB'"""
     return "HBNB"
 
 
 @app.route("/c/<text>", strict_slashes=False)
 def c(text):
-    """Displays 'C' followed by the value of <text>.
+    """Displays 'C' followed by the value of <text>
     Replaces any underscores in <text> with slashes.
     """
     text = text.replace("_", " ")
@@ -37,7 +39,7 @@ def c(text):
 @app.route("/python", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def python(text="is cool"):
-    """Displays 'Python' followed by the value of <text>.
+    """Displays 'Python' followed by the value of <text>
     Replaces any underscores in <text> with slashes.
     """
     text = text.replace("_", " ")
@@ -46,16 +48,14 @@ def python(text="is cool"):
 
 @app.route("/number/<int:n>", strict_slashes=False)
 def number(n):
-    """ Display 'n' if it is a number then value <n>.
-    """
+    """Displays 'n is a number' only if <n> is an integer."""
     return "{} is a number".format(n)
 
 
 @app.route("/number_template/<int:n>", strict_slashes=False)
 def number_template(n):
-    """ Display a HTML page only if n is an integer.
-    """
-    return render_template("5-number.html", n=n)       
+    """Displays an HTML page only if <n> is an integer."""
+    return render_template("5-number.html", n=n)
 
 
 if __name__ == "__main__":
